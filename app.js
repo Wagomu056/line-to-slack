8558980a75d0fa3eacf2fa37a424fa2e505ffc96
Takuya Azumaguchi <catchballer@gmail.com>
2023-06-25 13:51:40 +0900

    Change to transfer LINE message to Slack

--- app.js
+++ app.js
@@ -8,26 +8,57 @@ app.use(bodyParser.urlencoded({
 }));
 app.use(bodyParser.json());
 
+const token = process.env.TARGET_SLACK_OAUTH;
+const channel = process.env.TARGET_SLACK_CHANNEL;
+
 app.listen(3000);
 console.log('Server is online.');
 
 app.post('/line-webhook', function(req, res) {
     res.send('Slack send start');
 
-    // todo output body
-    console.log(req.body);
+    // Pars from body
+    const event = req.body.events[0];
+    if (event.type === 'message') {
+        const message = event.message;
+        if (message.type === 'text') {
+            const text = createTextByEvent(message.text, event.timestamp);
+            console.log(text);
 
+            sendToSlack(token, channel, text);
+        }
+        else {
+            let msg = 'その他タイプのメッセージです。[' + message.type + ']';
+            const text = createTextByEvent(msg, event.timestamp);
+            console.log(text);
+
+            sendToSlack(token, channel, text);
+        }
+    }
+})
+
+function createTextByEvent(message, timestamp) {
+    let text = 'LINEから転送\n';
+    text += '-----\n';
+    text += message;
+    text += '\n-----\n';
+    const date = new Date(timestamp);
+    text += date.toString();
+
+    return text;
+}
+
+function sendToSlack(oAuthToken, channel, message) {
     const { WebClient } = require('@slack/web-api');
-
     (async () => {
         const token = process.env.TARGET_SLACK_OAUTH;
         const channel = process.env.TARGET_SLACK_CHANNEL;
-        const text = '*Hello World*';
+        const text = message;
 
         const client = new WebClient(token);
         const response = await client.chat.postMessage({ channel, text });
 
         console.log(response.ok);
     })();
-})
+}
 
