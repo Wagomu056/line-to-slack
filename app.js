ff55f376aed1ac643c9fe19edd44221fc3d66f5f
Takuya Azumaguchi <catchballer@gmail.com>
2023-06-25 12:54:34 +0900

    Add body output of express

--- app.js
+++ app.js
@@ -1,12 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(bodyParser.json());
 
 app.listen(3000);
 console.log('Server is online.');
 
 app.post('/line-webhook', function(req, res) {
     res.send('Slack send start');
+
+    // todo output body
+    console.log(req.body);
 
     const { WebClient } = require('@slack/web-api');
 
