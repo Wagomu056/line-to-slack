e2f652dc66eec88aba99d170c071f8494a1f3edf
Takuya Azumaguchi <catchballer@gmail.com>
2023-06-25 12:54:11 +0900

    Change to use env vars

--- app.js
+++ app.js
@@ -11,19 +11,14 @@ app.post('/line-webhook', function(req, res) {
     const { WebClient } = require('@slack/web-api');
 
     (async () => {
-        // OAuth トークン
-        const token  = 'xoxb-1122712679271-5475292102069-wXQQ0utD7TcuKqD0zJkgp70x';
-        // #チャンネル名 of @ユーザー名
-        const channel = '#line_to_slack_test';
-        // メッセージ
+        const token = process.env.TARGET_SLACK_OAUTH;
+        const channel = process.env.TARGET_SLACK_CHANNEL;
         const text = '*Hello World*';
 
         const client = new WebClient(token);
         const response = await client.chat.postMessage({ channel, text });
 
-        // 投稿に成功すると `ok` フィールドに `true` が入る。
         console.log(response.ok);
-        // => true
     })();
 })
 
---
+++ run.sh
@@ -0,0 +1,1 @@
+env TARGET_SLACK_OAUTH="xoxb-1122712679271-5475292102069-wXQQ0utD7TcuKqD0zJkgp70x" TARGET_SLACK_CHANNEL="#line_to_slack_test" node app.js
